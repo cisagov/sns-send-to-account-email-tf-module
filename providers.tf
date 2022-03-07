0e79063482aedb10b124a53801c39abd51bf61e3
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-03-07 12:41:24 -0500

    First commits

--- providers.tf
+++ providers.tf
@@ -1,13 +1,5 @@
-# This is an example of what a provider looks like.
-#
-# provider "aws" {
-#   alias = "myprovider"
-#   assume_role {
-#     role_arn     = "arn:aws:iam::123456789012:role/MyRole"
-#     session_name = "MySessionName"
-#   }
-#   default_tags {
-#     tags = var.tags
-#   }
-#   region = var.aws_region
-# }
+# The provider is used to read account information (account IDs and
+# emails) from AWS Organizations.
+provider "aws" {
+  alias = "organizations_read_only"
+}
--- variables.tf
+++ variables.tf
@@ -1,33 +1,16 @@
 # ------------------------------------------------------------------------------
-# REQUIRED PARAMETERS
-#
-# You must provide a value for each of these parameters.
-# ------------------------------------------------------------------------------
-
-variable "subnet_id" {
-  type        = string
-  description = "The ID of the AWS subnet to deploy into (e.g. subnet-0123456789abcdef0)."
-}
-
-# ------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 #
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
-variable "ami_owner_account_id" {
+variable "topic_display_name" {
   type        = string
-  description = "The ID of the AWS account that owns the Example AMI, or \"self\" if the AMI is owned by the same account as the provisioner."
-  default     = "self"
+  description = "The display name of the SNS topic."
+  default     = "cloudwatch_alarms"
 }
 
-variable "aws_availability_zone" {
+variable "topic_name" {
   type        = string
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
-  default     = "a"
-}
-
-variable "aws_region" {
-  type        = string
-  description = "The AWS region to deploy into (e.g. us-east-1)."
-  default     = "us-east-1"
+  description = "The name of the SNS topic."
+  default     = "cloudwatch-alarms"
 }
