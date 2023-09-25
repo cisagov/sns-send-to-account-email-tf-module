93f5f4aef2b5dbc286760926888e062028566dba
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2023-09-25 15:40:37 -0400

    Alphabetize HCL attributes

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "sns_topic" {
-  value       = aws_sns_topic.this
   description = "The SNS topic to which a message can be sent to forward it on to the email associated with the account."
+  value       = aws_sns_topic.this
 }
--- variables.tf
+++ variables.tf
@@ -4,11 +4,11 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 variable "topic_display_name" {
-  type        = string
   description = "The display name of the SNS topic."
+  type        = string
 }
 
 variable "topic_name" {
-  type        = string
   description = "The name of the SNS topic."
+  type        = string
 }
